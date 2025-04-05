628baabdaa2dc11667087723e4f66d74de2aee8f
zezo-melo <robertomello404@gmail.com>
2025-04-05 15:11:38 -0300

    feat: change configs on server.js to deploy

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(express.json());
