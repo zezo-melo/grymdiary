a6d5bda02fcf6c8e7fb16b3c38b35943586afc31
zezo-melo <robertomello404@gmail.com>
2025-04-07 17:29:07 -0300

    fix: remove fallback from axiosInstance

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/",
+  // baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 axiosInstance.interceptors.response.use(
